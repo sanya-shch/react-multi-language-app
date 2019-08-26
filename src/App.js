f1cf75c728f84d9aae2af78b9b8b43654a31b439
sanya-shch <shchipkov.sanya@ukr.net>
2019-08-27 00:43:43 +0300

    add translate library

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
 import React from 'react';
+import counterpart from 'counterpart';
+import Translate from 'react-translate-component';
+
 import './App.css';
+
+counterpart.registerTranslations('en', {
+    title: 'Title',
+    paragraphs: {
+        p1: 'Paragraph with <b>html</b> inside',
+        p2: 'Paragraph with %(link)s'
+    },
+    link: 'link',
+    placeholder: 'input'
+});
+counterpart.setLocale('en');
+
+const Link = (props) =>
+    <Translate
+        content={props.content}
+        component="a"
+        href="//www.npmjs.com/package/react-translate-component"
+        target="_blank"
+    />;
 
 export default class App extends React.Component {
 
     render() {
+
+        const link = <Link content="link"/>;
+
         return (
             <div className="App">
 
-                <h1>Title</h1>
+                <Translate content="title" component="h1"/>
 
-                <p>
-                    Paragraph with <b>html</b> inside
-                </p>
+                <Translate content="paragraphs.p1" component="p" unsafe={true}/>
 
-                <p>
-                    Paragraph with <a href="//google.com" target="_blank">link</a>
-                </p>
+                <Translate content="paragraphs.p2" component="p" with={{link}}/>
 
-                <input type="text" placeholder="input"/>
+                <Translate component="input" attributes={{placeholder: 'placeholder'}}/>
+
             </div>
         );
     }
